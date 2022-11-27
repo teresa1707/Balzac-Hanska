493d976394148149bee7061a4770f5f0b1ed6a7f
Teresa <tvlviv@hotmail.fr>
2022-11-27 23:42:42 +0100

    adding slider

--- src/Container/Main/Main.js
+++ src/Container/Main/Main.js
@@ -1,89 +1,209 @@
-import React from "react";
+import React, {useEffect, useRef} from "react"
+import {gsap} from "gsap"
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import {ScrollToPlugin} from 'gsap/ScrollToPlugin'
+import SplitText from 'gsap/SplitText'
+import './Main.scss'
+
+gsap.registerPlugin(SplitText)
 
 export const Main = () => {
+const sections = document.querySelectorAll("section");
+const images = document.querySelectorAll(".bg");
+const headings = gsap.utils.toArray(".section-heading");
+const outerWrappers = gsap.utils.toArray(".outer");
+const innerWrappers = gsap.utils.toArray(".inner");
+
+document.addEventListener("wheel", handleWheel);
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchmove", handleTouchMove);
+document.addEventListener("touchend", handleTouchEnd);
+
+let listening = false,
+  direction = "down",
+  current,
+  next = 0;
+
+const touch = {
+  startX: 0,
+  startY: 0,
+  dx: 0,
+  dy: 0,
+  startTime: 0,
+  dt: 0
+};
+
+const tlDefaults = {
+  ease: "slow.inOut",
+  duration: 1.25
+};
+
+const splitHeadings = headings.map((heading) => {
+  return new SplitText(heading, {
+    type: "chars, words, lines",
+    linesClass: "clip-text"
+  });
+});
+
+function revealSectionHeading() {
+  return gsap.to(splitHeadings[next].chars, {
+    autoAlpha: 1,
+    yPercent: 0,
+    duration: 1,
+    ease: "power2",
+    stagger: {
+      each: 0.02,
+      from: "random"
+    }
+  });
+}
+
+gsap.set(outerWrappers, { yPercent: 100 });
+gsap.set(innerWrappers, { yPercent: -100 });
+
+// Slides a section in on scroll down
+function slideIn() {
+  // The first time this function runs, current is undefined
+  if (current !== undefined) gsap.set(sections[current], { zIndex: 0 });
+
+  gsap.set(sections[next], { autoAlpha: 1, zIndex: 1 });
+  gsap.set(images[next], { yPercent: 0 });
+  gsap.set(splitHeadings[next].chars, { autoAlpha: 0, yPercent: 100 });
+
+  const tl = gsap
+    .timeline({
+      paused: true,
+      defaults: tlDefaults,
+      onComplete: () => {
+        listening = true;
+        current = next;
+      }
+    })
+    .to([outerWrappers[next], innerWrappers[next]], { yPercent: 0 }, 0)
+    .from(images[next], { yPercent: 15 }, 0)
+    .add(revealSectionHeading(), 0);
+
+  if (current !== undefined) {
+    tl.add(
+      gsap.to(images[current], {
+        yPercent: -15,
+        ...tlDefaults
+      }),
+      0
+    ).add(
+      gsap
+        .timeline()
+        .set(outerWrappers[current], { yPercent: 100 })
+        .set(innerWrappers[current], { yPercent: -100 })
+        .set(images[current], { yPercent: 0 })
+        .set(sections[current], { autoAlpha: 0 })
+    );
+  }
+
+  tl.play(0);
+}
+
+// Slides a section out on scroll up
+function slideOut() {
+  gsap.set(sections[current], { zIndex: 1 });
+  gsap.set(sections[next], { autoAlpha: 1, zIndex: 0 });
+  gsap.set(splitHeadings[next].chars, { autoAlpha: 0, yPercent: 100 });
+  gsap.set([outerWrappers[next], innerWrappers[next]], { yPercent: 0 });
+  gsap.set(images[next], { yPercent: 0 });
+
+  gsap
+    .timeline({
+      defaults: tlDefaults,
+      onComplete: () => {
+        listening = true;
+        current = next;
+      }
+    })
+    .to(outerWrappers[current], { yPercent: 100 }, 0)
+    .to(innerWrappers[current], { yPercent: -100 }, 0)
+    .to(images[current], { yPercent: 15 }, 0)
+    .from(images[next], { yPercent: -15 }, 0)
+    .add(revealSectionHeading(), ">-1")
+    .set(images[current], { yPercent: 0 });
+}
+
+function handleDirection() {
+  listening = false;
+
+  if (direction === "down") {
+    next = current + 1;
+    if (next >= sections.length) next = 0;
+    slideIn();
+  }
+
+  if (direction === "up") {
+    next = current - 1;
+    if (next < 0) next = sections.length - 1;
+    slideOut();
+  }
+}
+
+function handleWheel(e) {
+  if (!listening) return;
+  direction = e.wheelDeltaY < 0 ? "down" : "up";
+  handleDirection();
+}
+
+function handleTouchStart(e) {
+  if (!listening) return;
+  const t = e.changedTouches[0];
+  touch.startX = t.pageX;
+  touch.startY = t.pageY;
+}
+
+function handleTouchMove(e) {
+  if (!listening) return;
+  e.preventDefault();
+}
+
+function handleTouchEnd(e) {
+  if (!listening) return;
+  const t = e.changedTouches[0];
+  touch.dx = t.pageX - touch.startX;
+  touch.dy = t.pageY - touch.startY;
+  if (touch.dy > 10) direction = "up";
+  if (touch.dy < -10) direction = "down";
+  handleDirection();
+}
+
+slideIn();
+
     return (
         <>
-        {/* Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…)1. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku.
+        <section className="first">
+  <div className="outer">
+    <div className="inner">
+      <div className="bg one">
+        <h2 className="section-heading">Scroll down</h2>
+      </div>
+    </div>
+  </div>
 
-L'étude formelle des textes s'appuie sur la linguistique, qui est l'approche scientifique du langage.
+</section>
+<section className="second">
+  <div className="outer">
+    <div className="inner">
+      <div className="bg">
+        <h2 className="section-heading">Animated with GSAP</h2>
+      </div>
+    </div>
+  </div>
+</section>
+<section className="third">
+  <div className="outer">
+    <div className="inner">
+      <div className="bg">
+        <h2 className="section-heading">GreenSock</h2>
+      </div>
+    </div>
+  </div>
+</section>
 
-Étymologie
-« Texte » est issu du mot latin « textum », dérivé du verbe « texere » qui signifie « tisser ». Le mot s'applique à l'entrelacement des fibres utilisées dans le tissage, voir par exemple Ovide : « Quo super iniecit textum rude sedula Baucis = (un siège) sur lequel Baucis empressée avait jeté un tissu grossier »2 ou au tressage (exemple chez Martial « Vimineum textum = panier d'osier tressé »). Le verbe a aussi le sens large de construire comme dans « basilicam texere = construire une basilique » chez Cicéron3.
 
-Le sens figuré d'éléments de langage organisés et enchaînés apparaît avant l'Empire romain : il désigne un agencement particulier du discours. Exemple : « epistolas texere = composer des épîtres » - Cicéron (ier siècle av. J.-C.)4 ou plus nettement chez Quintilien (ier siècle apr. J.-C.) : « verba in textu jungantur = l'agencement des mots dans la phrase »5.
-
-Les formes anciennes du Moyen Âge désignent au xiie siècle le volume qui contient le texte sacré des Évangiles, puis au xiiie siècle, le texte original d'un livre saint ou des propos de quelqu'un. Au xviie siècle le mot s’applique au passage d'un ouvrage pris comme référence et au début du xixe siècle le mot texte a son sens général d'« écrit »6.
-
-Texte littéraire
-Un texte répond de façon plus ou moins pertinente à des critères qui en déterminent la qualité littéraire. On retient en particulier la structure d'ensemble, la syntaxe et la ponctuation, l'orthographe lexicale et grammaticale, la pertinence et la richesse du vocabulaire, la présence de figures de style, le registre de langue et la fonction recherchée (narrative, descriptive, expressive, argumentative, injonctive, poétique). C'est l'objet de l'analyse littéraire.
-
-En informatique
-En informatique, la notion de texte s'oppose à la notion de données binaires, on parle alors :
-
-de texte brut dans un protocole de télécommunication ;
-de document texte si le logiciel utilisé permet de formater le texte ;
-ou encore de texte formaté ou riche lorsque les indications de formats sont données en texte brut (exemple : Rich Text Format) ;
-de fichier texte dans un fichier qui ne contient que des caractères sans mise en forme autre que les sauts de lignes et le choix des caractères.
-Notes et références
-François Rastier définit le texte comme « une suite linguistique autonome (orale ou écrite) constituant une unité empirique, et produite par un ou plusieurs énonciateurs dans une pratique sociale attestée » (François Rastier, 2001, Arts et sciences du texte, Paris, PUF, page 302).
-Métamorphoses 8, 640 [archive]
-Paullus in medio foro jam paene basilicam texuit iisdem antiquis columnis [1] [archive] Ciceron, Ad Atticum, 4,16, 14, Lettre CXLIX - page 306
-« epistolas quotidianis verbis texere solemus » Ciceron, Lettres : Ad Familiares, IX, 21, lettre DCLVIII [2] [archive]
-« quantumque interest, sensus idem quibus verbis efferatur, tantum, verba eadem qua in compositione vel in textu jungantur = (autant la valeur de la pensée varie suivant les mots qui l'expriment, autant celle des mots varie selon leur harmonie et selon leur agencement) - Quintilien De l'Institution oratoire Livre IX, ch. 4 De compositione (De l'arrangement des mots) [3] [archive] page 326, ou page 392 ibidem « Historia non tam finitos numeros, quam orbem quemdam contextumque desiderat : namque omnia ejus membra connexa sunt, quoniam lubrica est, ac fluit » page 392 = « L'histoire n'exige pas tant une prose parfaitement cadencée, qu'une suite de phrases dont le tissu soit bien lié; car tout s'y enchaîne, tout y coule, tout y glisse. »
-Dictionnaire CNRTL [archive]Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…)1. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku.
-
-L'étude formelle des textes s'appuie sur la linguistique, qui est l'approche scientifique du langage.
-
-Étymologie
-« Texte » est issu du mot latin « textum », dérivé du verbe « texere » qui signifie « tisser ». Le mot s'applique à l'entrelacement des fibres utilisées dans le tissage, voir par exemple Ovide : « Quo super iniecit textum rude sedula Baucis = (un siège) sur lequel Baucis empressée avait jeté un tissu grossier »2 ou au tressage (exemple chez Martial « Vimineum textum = panier d'osier tressé »). Le verbe a aussi le sens large de construire comme dans « basilicam texere = construire une basilique » chez Cicéron3.
-
-Le sens figuré d'éléments de langage organisés et enchaînés apparaît avant l'Empire romain : il désigne un agencement particulier du discours. Exemple : « epistolas texere = composer des épîtres » - Cicéron (ier siècle av. J.-C.)4 ou plus nettement chez Quintilien (ier siècle apr. J.-C.) : « verba in textu jungantur = l'agencement des mots dans la phrase »5.
-
-Les formes anciennes du Moyen Âge désignent au xiie siècle le volume qui contient le texte sacré des Évangiles, puis au xiiie siècle, le texte original d'un livre saint ou des propos de quelqu'un. Au xviie siècle le mot s’applique au passage d'un ouvrage pris comme référence et au début du xixe siècle le mot texte a son sens général d'« écrit »6.
-
-Texte littéraire
-Un texte répond de façon plus ou moins pertinente à des critères qui en déterminent la qualité littéraire. On retient en particulier la structure d'ensemble, la syntaxe et la ponctuation, l'orthographe lexicale et grammaticale, la pertinence et la richesse du vocabulaire, la présence de figures de style, le registre de langue et la fonction recherchée (narrative, descriptive, expressive, argumentative, injonctive, poétique). C'est l'objet de l'analyse littéraire.
-
-En informatique
-En informatique, la notion de texte s'oppose à la notion de données binaires, on parle alors :
-
-de texte brut dans un protocole de télécommunication ;
-de document texte si le logiciel utilisé permet de formater le texte ;
-ou encore de texte formaté ou riche lorsque les indications de formats sont données en texte brut (exemple : Rich Text Format) ;
-de fichier texte dans un fichier qui ne contient que des caractères sans mise en forme autre que les sauts de lignes et le choix des caractères.
-Notes et références
-François Rastier définit le texte comme « une suite linguistique autonome (orale ou écrite) constituant une unité empirique, et produite par un ou plusieurs énonciateurs dans une pratique sociale attestée » (François Rastier, 2001, Arts et sciences du texte, Paris, PUF, page 302).
-Métamorphoses 8, 640 [archive]
-Paullus in medio foro jam paene basilicam texuit iisdem antiquis columnis [1] [archive] Ciceron, Ad Atticum, 4,16, 14, Lettre CXLIX - page 306
-« epistolas quotidianis verbis texere solemus » Ciceron, Lettres : Ad Familiares, IX, 21, lettre DCLVIII [2] [archive]
-« quantumque interest, sensus idem quibus verbis efferatur, tantum, verba eadem qua in compositione vel in textu jungantur = (autant la valeur de la pensée varie suivant les mots qui l'expriment, autant celle des mots varie selon leur harmonie et selon leur agencement) - Quintilien De l'Institution oratoire Livre IX, ch. 4 De compositione (De l'arrangement des mots) [3] [archive] page 326, ou page 392 ibidem « Historia non tam finitos numeros, quam orbem quemdam contextumque desiderat : namque omnia ejus membra connexa sunt, quoniam lubrica est, ac fluit » page 392 = « L'histoire n'exige pas tant une prose parfaitement cadencée, qu'une suite de phrases dont le tissu soit bien lié; car tout s'y enchaîne, tout y coule, tout y glisse. »
-Dictionnaire CNRTL [archive]Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…)1. Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku.
-
-L'étude formelle des textes s'appuie sur la linguistique, qui est l'approche scientifique du langage.
-
-Étymologie
-« Texte » est issu du mot latin « textum », dérivé du verbe « texere » qui signifie « tisser ». Le mot s'applique à l'entrelacement des fibres utilisées dans le tissage, voir par exemple Ovide : « Quo super iniecit textum rude sedula Baucis = (un siège) sur lequel Baucis empressée avait jeté un tissu grossier »2 ou au tressage (exemple chez Martial « Vimineum textum = panier d'osier tressé »). Le verbe a aussi le sens large de construire comme dans « basilicam texere = construire une basilique » chez Cicéron3.
-
-Le sens figuré d'éléments de langage organisés et enchaînés apparaît avant l'Empire romain : il désigne un agencement particulier du discours. Exemple : « epistolas texere = composer des épîtres » - Cicéron (ier siècle av. J.-C.)4 ou plus nettement chez Quintilien (ier siècle apr. J.-C.) : « verba in textu jungantur = l'agencement des mots dans la phrase »5.
-
-Les formes anciennes du Moyen Âge désignent au xiie siècle le volume qui contient le texte sacré des Évangiles, puis au xiiie siècle, le texte original d'un livre saint ou des propos de quelqu'un. Au xviie siècle le mot s’applique au passage d'un ouvrage pris comme référence et au début du xixe siècle le mot texte a son sens général d'« écrit »6.
-
-Texte littéraire
-Un texte répond de façon plus ou moins pertinente à des critères qui en déterminent la qualité littéraire. On retient en particulier la structure d'ensemble, la syntaxe et la ponctuation, l'orthographe lexicale et grammaticale, la pertinence et la richesse du vocabulaire, la présence de figures de style, le registre de langue et la fonction recherchée (narrative, descriptive, expressive, argumentative, injonctive, poétique). C'est l'objet de l'analyse littéraire.
-
-En informatique
-En informatique, la notion de texte s'oppose à la notion de données binaires, on parle alors :
-
-de texte brut dans un protocole de télécommunication ;
-de document texte si le logiciel utilisé permet de formater le texte ;
-ou encore de texte formaté ou riche lorsque les indications de formats sont données en texte brut (exemple : Rich Text Format) ;
-de fichier texte dans un fichier qui ne contient que des caractères sans mise en forme autre que les sauts de lignes et le choix des caractères.
-Notes et références
-François Rastier définit le texte comme « une suite linguistique autonome (orale ou écrite) constituant une unité empirique, et produite par un ou plusieurs énonciateurs dans une pratique sociale attestée » (François Rastier, 2001, Arts et sciences du texte, Paris, PUF, page 302).
-Métamorphoses 8, 640 [archive]
-Paullus in medio foro jam paene basilicam texuit iisdem antiquis columnis [1] [archive] Ciceron, Ad Atticum, 4,16, 14, Lettre CXLIX - page 306
-« epistolas quotidianis verbis texere solemus » Ciceron, Lettres : Ad Familiares, IX, 21, lettre DCLVIII [2] [archive]
-« quantumque interest, sensus idem quibus verbis efferatur, tantum, verba eadem qua in compositione vel in textu jungantur = (autant la valeur de la pensée varie suivant les mots qui l'expriment, autant celle des mots varie selon leur harmonie et selon leur agencement) - Quintilien De l'Institution oratoire Livre IX, ch. 4 De compositione (De l'arrangement des mots) [3] [archive] page 326, ou page 392 ibidem « Historia non tam finitos numeros, quam orbem quemdam contextumque desiderat : namque omnia ejus membra connexa sunt, quoniam lubrica est, ac fluit » page 392 = « L'histoire n'exige pas tant une prose parfaitement cadencée, qu'une suite de phrases dont le tissu soit bien lié; car tout s'y enchaîne, tout y coule, tout y glisse. »
-Dictionnaire CNRTL [archive] */}
           </>
           )}
